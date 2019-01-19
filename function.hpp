e64342221566f1372f16f8d3126565098175e964
operasfantom <operasfantom@gmail.com>
2019-01-19 19:21:42 +0300

    init

--- function.hpp
+++ function.hpp
@@ -3,31 +3,49 @@
 
 #include <cstddef>
 #include <memory>
-
+#include <variant>
+#include <vector>
+#include <iostream>
 
 template<typename T>
 class function;
 
 template<typename R, typename... Args>
 class function<R(Args...)> {
-//    template<typename R, typename... Args>
     class concept {
     public:
-        virtual R operator()(Args &&...args) const = 0;
+        virtual ~concept() = default;
+
+        virtual R operator()(Args &&...args)/* const */= 0;
     };
 
-    template<typename F/*, typename R, typename... Args*/>
-    class model : public concept/*<R, Args...>*/ {
+    template<typename F>
+    class model : public concept {
         F f;
     public:
         explicit model(F &&f) : f(std::forward<F>(f)) {}
 
-        R operator()(Args &&... args) const override {
+        R operator()(Args &&... args)/* const */override {
             return f(std::forward<Args>(args)...);
         }
     };
 
-    std::shared_ptr<concept/*<R, Args...>*/> ptr;
+    using word_t = std::byte;
+
+    constexpr static size_t FIXED_SIZE = 16;
+
+    std::array<word_t, FIXED_SIZE> data;
+    size_t offset = 0;
+
+    std::shared_ptr<concept> ptr;
+
+    bool small = false;
+
+    void set_pointer_to_data() {
+        auto concept_ptr = reinterpret_cast<concept *>(data.data() + offset);
+        ptr = std::shared_ptr<concept>(concept_ptr, [](concept *) {});
+    }
+
 public:
     function() noexcept;
 
@@ -67,19 +85,39 @@ function<R(Args...)>::function(std::nullptr_t) noexcept {
 }
 
 template<typename R, typename... Args>
-function<R(Args...)>::function(function const &other) : ptr(other.ptr) {
-
+function<R(Args...)>::function(function const &other) : data(other.data), offset(other.offset), small(other.small) {
+    if (small) {
+        this->set_pointer_to_data();
+    } else {
+        ptr = other.ptr;
+    }
 }
 
 template<typename R, typename... Args>
-function<R(Args...)>::function(function &&other) noexcept : ptr(std::move(other.ptr)) {
-
+function<R(Args...)>::function(function &&other) noexcept : data(std::move(other.data)), offset(other.offset), small(other.small) {
+    if (small) {
+        this->set_pointer_to_data();
+    } else {
+        ptr = std::move(other.ptr);
+    }
 }
 
 template<typename R, typename... Args>
 template<typename F>
-function<R(Args...)>::function(F &&f) : ptr(std::make_shared<function::model<F>>(std::forward<F>(f))) {
+function<R(Args...)>::function(F &&f) {
+    using concept_t = function::concept;
+    using model_t = function::model<F>;
+    if (sizeof(std::decay_t<F>) * 2 <= FIXED_SIZE) {
+        auto model_ptr = new(reinterpret_cast<model_t *>(data.data())) model_t(std::forward<F>(f));
+        offset = model_ptr - reinterpret_cast<model_t *>(data.data());
+        ptr = std::shared_ptr<model_t>(model_ptr, [](model_t *) {});
 
+        small = true;
+    } else {
+        ptr = std::make_shared<model_t>(std::forward<F>(f));
+
+        small = false;
+    }
 }
 
 template<typename R, typename... Args>
@@ -89,24 +127,36 @@ function<R(Args...)>::~function() {
 
 template<typename R, typename... Args>
 function<R(Args...)> &function<R(Args...)>::operator=(const function &other) {
-    ptr = other.ptr;
+    auto tmp(other);
+    tmp.swap(*this);
     return *this;
 }
 
 template<typename R, typename... Args>
 function<R(Args...)> &function<R(Args...)>::operator=(function &&other) noexcept {
-    ptr = std::move(other.ptr);
+    auto tmp(std::move(other));
+    tmp.swap(*this);
     return *this;
 }
 
 template<typename R, typename... Args>
 void function<R(Args...)>::swap(function &other) noexcept {
+    std::swap(data, other.data);
+    std::swap(offset, other.offset);
     std::swap(ptr, other.ptr);
+    std::swap(small, other.small);
+
+    if (this->small) {
+        this->set_pointer_to_data();
+    }
+    if (other.small) {
+        other.set_pointer_to_data();
+    }
 }
 
 template<typename R, typename... Args>
 function<R(Args...)>::operator bool() const noexcept {
-    return ptr;
+    return ptr.get() != nullptr;
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -10,13 +10,20 @@ auto print_ptr = &print;
 
 int main() {
 //    function<void(int x)> f;
+    int y = 2;
     function<void(std::ostream &os, int x)> f;
-    auto lambda = [](std::ostream &os, int x) mutable {
-        os << x << std::endl;
+    auto lambda = [y](std::ostream &os, int x) mutable {
+        os << x << ' ' << y << std::endl;
     };
     f = lambda;
     f(std::cout, 1);
 
     f = print_ptr;
     f(std::cout, 2);
+
+    std::vector<int> large_data(1000, -1);
+    function<void(std::ostream &)> g = [large_data](std::ostream &os) {
+        os << "LARGE:" << large_data[0] << std::endl;
+    };
+    g(std::cerr);
 }
