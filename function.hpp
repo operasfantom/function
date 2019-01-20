6dbd2f08d6a4188694456d0d01d7ade3c268ddc6
operasfantom <operasfantom@gmail.com>
2019-01-20 18:19:00 +0300

    some fixes

--- function.hpp
+++ function.hpp
@@ -12,18 +12,15 @@ class function;
 
 template<typename R, typename... Args>
 class function<R(Args...)> {
-    using word_t = std::byte;
-    using pointer_t = word_t *;
-
     class concept {
     public:
         virtual ~concept() = default;
 
         virtual R operator()(Args &&...args)/* const */= 0;
 
-        virtual pointer_t copy(pointer_t) = 0;
+        virtual concept *copy(std::byte *) = 0;
 
-        virtual pointer_t move(pointer_t) = 0;
+        virtual concept *move(std::byte *) = 0;
     };
 
     template<typename F>
@@ -34,45 +31,59 @@ class function<R(Args...)> {
 
         R operator()(Args &&... args)/* const */override;
 
-        pointer_t copy(pointer_t destination) override;
+        concept *copy(std::byte *destination) override;
 
-        pointer_t move(pointer_t pointer) override;
+        concept *move(std::byte *pointer) override;
     };
 
 
     template<typename F>
-    static std::enable_if_t<!std::is_copy_constructible_v<F>, pointer_t> copy_model(F const &f, pointer_t destination) {
+    static std::enable_if_t<!std::is_copy_constructible_v<F>, concept *> copy_model(F const &f, std::byte *destination) {
         return nullptr;
     }
 
     template<typename F>
-    static std::enable_if_t<std::is_copy_constructible_v<F>, pointer_t> copy_model(F const &f, pointer_t destination) {
-        return reinterpret_cast<pointer_t >(::new(reinterpret_cast<model<F> *>(destination)) model<F>(f));
+    static std::enable_if_t<std::is_copy_constructible_v<F>, concept *> copy_model(F const &f, std::byte *destination) {
+        return static_cast<concept *>(::new(reinterpret_cast<model<F> *>(destination)) model<F>(f));
     }
 
     template<typename F>
-    static std::enable_if_t<!std::is_move_constructible_v<F>, pointer_t> move_model(F &&f, pointer_t destination) {
+    static std::enable_if_t<!std::is_move_constructible_v<F>, concept *> move_model(F &&f, std::byte *destination) {
         return nullptr;
     }
 
     template<typename F>
-    static std::enable_if_t<std::is_move_constructible_v<F>, pointer_t> move_model(F &&f, pointer_t destination) {
-        return reinterpret_cast<pointer_t >(::new(reinterpret_cast<model<F> *>(destination)) model<F>(std::move(f)));
+    static std::enable_if_t<std::is_move_constructible_v<F>, concept *> move_model(F &&f, std::byte *destination) {
+        return static_cast<concept * >(::new(reinterpret_cast<model<F> *>(destination)) model<F>(std::move(f)));
     }
 
 
     constexpr static size_t FIXED_SIZE = 32;
 
-    std::array<word_t, FIXED_SIZE> data;
+    alignas(FIXED_SIZE * sizeof(std::byte)) std::array<std::byte, FIXED_SIZE> data;
     size_t offset = 0;
 
     std::shared_ptr<concept> shared_ptr;
     concept *ptr = nullptr;
 
-    bool small = false;
+//    bool small = false;
 
-    void set_pointer_to_data() {
-        ptr = reinterpret_cast<concept *>(data.data() + offset);
+    void reset_pointer() {
+        if (is_small()) {
+            if (ptr) {
+                ptr = reinterpret_cast<concept *>(data.data() + offset);
+            }
+        } else {
+            ptr = shared_ptr.get();
+        }
+    }
+
+    bool has_value() const {
+        return ptr != nullptr;
+    }
+
+    bool is_small() const {
+        return shared_ptr == nullptr;
     }
 
 public:
@@ -111,14 +122,14 @@ R function<R(Args...)>::model<F>::operator()(Args &&... args) {
 
 template<typename R, typename... Args>
 template<typename F>
-std::byte *function<R(Args...)>::model<F>::copy(std::byte *destination) {
-    return copy_model<F>(f, destination);
+typename function<R(Args...)>::concept *function<R(Args...)>::model<F>::copy(std::byte *destination) {
+    return copy_model(f, destination);
 }
 
 template<typename R, typename... Args>
 template<typename F>
-std::byte *function<R(Args...)>::model<F>::move(std::byte *destination) {
-    return move_model<std::decay_t<F>>(std::move(f), destination);
+typename function<R(Args...)>::concept *function<R(Args...)>::model<F>::move(std::byte *destination) {
+    return move_model(std::move(f), destination);
 }
 
 template<typename R, typename... Args>
@@ -132,10 +143,9 @@ function<R(Args...)>::function(std::nullptr_t) noexcept {
 }
 
 template<typename R, typename... Args>
-function<R(Args...)>::function(function const &other) : offset(other.offset), small(other.small) {
-    if (small) {
-        other.ptr->copy(data.data());
-        this->set_pointer_to_data();
+function<R(Args...)>::function(function const &other) : offset(other.offset) {
+    if (other.is_small()) {
+        ptr = other.ptr->copy(data.data());
     } else {
         shared_ptr = other.shared_ptr;
         ptr = shared_ptr.get();
@@ -143,10 +153,9 @@ function<R(Args...)>::function(function const &other) : offset(other.offset), sm
 }
 
 template<typename R, typename... Args>
-function<R(Args...)>::function(function &&other) noexcept : offset(other.offset), small(other.small) {
-    if (small) {
-        other.ptr->move(data.data());
-        this->set_pointer_to_data();
+function<R(Args...)>::function(function &&other) noexcept : offset(other.offset) {
+    if (other.is_small()) {
+        ptr = other.ptr->move(data.data());
     } else {
         shared_ptr = std::move(other.shared_ptr);
         ptr = shared_ptr.get();
@@ -156,28 +165,19 @@ function<R(Args...)>::function(function &&other) noexcept : offset(other.offset)
 template<typename R, typename... Args>
 template<typename F>
 function<R(Args...)>::function(F &&f) {
-    using concept_t = function::concept;
     using model_t = function::model<F>;
     if (sizeof(std::decay_t<F>) * 2 <= FIXED_SIZE) {
-        auto model_ptr = new(reinterpret_cast<model_t *>(data.data())) model_t(std::forward<F>(f));
-        offset = model_ptr - reinterpret_cast<model_t *>(data.data());
-        ptr = model_ptr;
-        /*shared_ptr = std::shared_ptr<model_t>(model_ptr, [](model_t *ptr) {
-            ptr->destruct();
-        });*/
-
-        small = true;
+        ptr = ::new(reinterpret_cast<model_t *>(data.data())) model_t(std::forward<F>(f));
+        offset = static_cast<size_t>(std::distance(data.data(), reinterpret_cast<std::byte *>(ptr)));
     } else {
         shared_ptr = std::make_shared<model_t>(std::forward<F>(f));
         ptr = shared_ptr.get();
-
-        small = false;
     }
 }
 
 template<typename R, typename... Args>
 function<R(Args...)>::~function() {
-    if (small) {
+    if (is_small() && has_value()) {
         ptr->~concept();
     }
 }
@@ -185,14 +185,14 @@ function<R(Args...)>::~function() {
 template<typename R, typename... Args>
 function<R(Args...)> &function<R(Args...)>::operator=(const function &other) {
     auto tmp(other);
-    tmp.swap(*this);
+    swap(tmp);
     return *this;
 }
 
 template<typename R, typename... Args>
 function<R(Args...)> &function<R(Args...)>::operator=(function &&other) noexcept {
     auto tmp(std::move(other));
-    tmp.swap(*this);
+    swap(tmp);
     return *this;
 }
 
@@ -202,19 +202,14 @@ void function<R(Args...)>::swap(function &other) noexcept {
     std::swap(offset, other.offset);
     std::swap(shared_ptr, other.shared_ptr);
     std::swap(ptr, other.ptr);
-    std::swap(small, other.small);
 
-    if (this->small) {
-        this->set_pointer_to_data();
-    }
-    if (other.small) {
-        other.set_pointer_to_data();
-    }
+    this->reset_pointer();
+    other.reset_pointer();
 }
 
 template<typename R, typename... Args>
 function<R(Args...)>::operator bool() const noexcept {
-    return ptr != nullptr;
+    return has_value();
 }
 
 #endif
